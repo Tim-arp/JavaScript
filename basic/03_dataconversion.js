9c760d3cb72d3372c5054cd738f418a6fb7a52de
Pramit Regmi <134371135+Tim-arp@users.noreply.github.com>
2024-07-04 03:00:01 +0000

    DataConversion

---
+++ basic/03_dataconversion.js
@@ -0,0 +1,55 @@
+/*
+let marks = "56"
+console.log(typeof marks);
+
+let valueInNumber = Number(marks) //it changes the datatypes into number
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+*/
+
+/*
+let score = "34ac"
+console.log(typeof score);
+
+let ScoreNumber = Number(score)
+console.log(typeof ScoreNumber) 
+console.log(ScoreNumber); 
+// here the datatype is changed into number but the value of string is not changed into number(it produces NaN as output)
+*/
+
+/*
+let score = null
+console.log(typeof score)
+
+let ScoreInNumber = Number(score)
+console.log(typeof ScoreInNumber)
+console.log(ScoreInNumber)
+//It converts the null datatype into number but the value of null will be produced as 0
+*/
+
+/*
+let score = undefined
+console.log(typeof score)
+
+let ScoreInNumber = Number(score)
+console.log(typeof ScoreInNumber)
+console.log(ScoreInNumber)
+                //OR
+let score 
+console.log(score)
+
+let ScoreInNumber = Number(score)
+console.log(typeof ScoreInNumber)
+console.log(ScoreInNumber)
+//It converts the undefined datatype into number but the value of undefined will be produced as NaN
+*/
+
+/*
+let score = true
+console.log(typeof score)
+
+let ScoreInNumber = Number(score)
+console.log(typeof ScoreInNumber)
+console.log(ScoreInNumber)
+//It converts boolean into number but the value of boolean will be converted as 1
+*/
