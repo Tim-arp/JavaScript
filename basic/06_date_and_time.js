db04dae11170dc651891a4fb207307d56df65105
Pramit Regmi <134371135+Tim-arp@users.noreply.github.com>
2024-07-07 17:20:24 +0000

    DateAndTime

---
+++ basic/06_date_and_time.js
@@ -0,0 +1,33 @@
+// date are calculated in miliseconds from 1970 jan 01
+
+// let myDate = new Date
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+
+// month 0 bata start hunxa
+// dd/mm/yy bata garda month 1 bata start hunxa
+
+// const CreatedDate = new Date (2024,4,12)
+// console.log(CreatedDate.toDateString());
+
+// const CreatedDateAndTime = new Date (2024,4,12,3,45)
+// console.log(CreatedDateAndTime.toLocaleString());
+
+// const newdate = new Date("12-02-2024")
+// console.log(newdate.toLocaleString("en-US"));
+
+
+// let timestamp = Date.now()
+// console.log(timestamp); date haru milisecond ma aauxa
+// console.log(CreatedDate.getTime()); given date milisecond ma aauxa 
+// console.log(Math.floor(timestamp/1000));
+
+let date = new Date
+console.log(date);
+console.log(date.getDay());
+console.log(date.getMonth());
