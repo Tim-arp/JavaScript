ede500db282f9205557977940d0e83b2035f905c
Pramit Regmi <134371135+Tim-arp@users.noreply.github.com>
2024-07-08 02:44:11 +0000

    Incomplete Array

---
+++ basic/07_array.js
@@ -0,0 +1,10 @@
+// const arr = [23,12,54,21]
+// console.log(arr);
+// const arr1 = new Array(12,67,23,2)
+// console.log(arr1[2]);
+
+const myarray = [23,67,26,89,233]
+myarray.push(86)
+console.log(myarray); 
+// console.log(myarray.push(90)); yesto use garda latest array ko index dinxa
+console.log(myarray);
