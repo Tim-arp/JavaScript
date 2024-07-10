ee72120b0dc6eb6aecd7450b64f282a6fd78218b
Pramit Regmi <134371135+Tim-arp@users.noreply.github.com>
2024-07-10 06:30:17 +0000

    incomplete array

--- basic/07_array.js
+++ basic/07_array.js
@@ -3,8 +3,16 @@
 // const arr1 = new Array(12,67,23,2)
 // console.log(arr1[2]);
 
-const myarray = [23,67,26,89,233]
-myarray.push(86)
-console.log(myarray); 
+// const myarray = [23,67,26,89,233]
+// myarray.push(86)
+// console.log(myarray); 
 // console.log(myarray.push(90)); yesto use garda latest array ko index dinxa
-console.log(myarray);
+// myarray.pop() pop out the last element of the array 
+// myarray.unshift(12) array ko start ma element add hunxa
+// myarray.shift() array ko first bata element remove garxa
+// console.log(myarray.includes(5)); array ma tyo element xa kinai bhanera show garxa
+// console.log(myarray.indexOf(67)); element ko positon dinxa
+
+
+// const newarray = myarray.join(); newarray ma myarray ko value janxa tara string datatype bhayera
+// console.log(newarray)
