f4a74c87f21cfdc1eba78332ad00402fa271e2fb
Pramit Regmi <134371135+Tim-arp@users.noreply.github.com>
2024-07-25 14:03:34 +0000

    Functions

--- basic/09_function.js
+++ basic/09_function.js
@@ -34,12 +34,59 @@
 // console.log(login("Timarp"));
 
 
-function login(username){
-    if(username === undefined){
-        console.log("Please enter a username");
-        return;
-    }
-    return `${username} just logged in`
-}
-console.log(login("Timarp"));
+// function login(username){
+//     if(username === undefined){
+//         console.log("Please enter a username");
+//         return;
+//     }
+//     return `${username} just logged in`
+// }
+// console.log(login("Timarp"));
 // console.log(login());
+
+
+/*
+function TotalPrice(...num1){
+    return num1;
+}
+console.log(TotalPrice(200,454,121));
+
+rest/spread operator use garna parxa(...)
+yo use garepar eutai parameter le jati ota pani element linxa as a array ko form ma 
+*/
+
+
+
+                // FUNCTIONS WITH OBJECTS
+/*
+
+const user = {
+    username : "Timarp",
+    id : 455
+}
+
+function HandleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and id is ${anyobject.id}` );
+}
+HandleObject(user);
+*/
+
+/*
+function HandleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and id is ${anyobject.id}` );
+}
+HandleObject({
+    username : "Pramit",
+    id : 432
+})
+*/
+
+            // FUNCTION WITH ARRAY 
+
+const MyNewArray = [322,656,23,6,11]
+
+function returnvalue(getarray){
+    return getarray[2];
+} 
+// console.log(returnvalue(MyNewArray));
+console.log(returnvalue([232,5454,323,6767,344]));
