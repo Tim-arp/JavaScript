e3b553dcb3987d0844619e7517226a1583dec054
Pramit Regmi <134371135+Tim-arp@users.noreply.github.com>
2024-07-26 01:30:53 +0000

    Scope

---
+++ basic/10_scope.js
@@ -0,0 +1,45 @@
+/*
+function one(){
+    const user = "Timarp";
+
+function two(){
+    const website = "youtube";
+    console.log(user);
+}
+
+    // console.log(website);
+    two()
+}
+one()
+
+// In this function one cannot take value from function two
+// but function two can take value from function one 
+*/
+
+/*
+if (true){
+    const user = "Pramit"
+    if (user === "Pramit"){
+        const website = " Youtube"
+        console.log(user + website);
+    }
+    // console.log(website);
+}
+// console.log(user);
+*/
+/*
+function one(num){
+    return num + 1
+}
+console.log(one(5))
+
+yo function ma function call garda ja call garda ni hunxa mathi tala anywhere 
+*/
+
+/*
+const two = function add(num){
+    return num + 2;
+}
+console.log(two(5));
+// yo function ma yedi function call mathi garyo bhani error dinxa
+*/
