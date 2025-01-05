d874f9ee6f469d7c1a1499e47b9fd7f3cf78a363
Pramit Regmi <134371135+Tim-arp@users.noreply.github.com>
2025-01-05 19:42:24 +0000

    Loops

---
+++ basic/14_loops.js
@@ -0,0 +1,40 @@
+// FOR 
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     console.log(element);
+// }
+
+
+// for(i=1;i<=10;i++){
+//     console.log(`Table of ${i} is `);
+//  for(j=1;j<=10;j++)
+// console.log(`${i} * ${j} = ${i * j}`);
+// console.log("\n")
+// }
+
+
+// let myArray = ["Ironman","Thor","Spiderman","Hulk"]
+// console.log(myArray.length);
+
+// for (let i = 0; i < myArray.length; i++) {
+//     const element = myArray[i];
+//     console.log(element);
+// }
+
+
+// for (let i = 1; i <= 30; i++) {
+//     if (i==10) {
+//         console.log(i);
+//         console.log("10 detected");
+//         break;        
+//     }
+//     console.log(i);
+// }
+
+for (let i = 0; i<=20; i++) {
+    if (i==10) {
+        console.log("10 detected");
+        continue
+    }
+    console.log(i);
+}
